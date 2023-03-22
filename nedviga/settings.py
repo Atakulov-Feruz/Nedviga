9469b1f8572a06b9b78b900646f67472c0261e00
Atakulov-Feruz <AtakulovFeruz05@gmail.com>
2023-03-22 20:22:39 +0500

    'changes1'

--- nedviga/settings.py
+++ nedviga/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'nedviga.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Nedviga',
+        'USER': 'postgres',
+        'PASSWORD': '20051109',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
